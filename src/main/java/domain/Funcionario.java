cd67c6934abb6948f918f4b39d297283fec96a94
Pedro Melo <ppvitorr@gmail.com>
2020-09-14 18:17:16 -0300

    Create domain entity classes

---
+++ src/main/java/domain/Funcionario.java
@@ -0,0 +1,51 @@
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@SuppressWarnings("serial")
+@Entity
+public class Funcionario extends GenericDomain {
+	
+	@Column(length = 50, nullable = false)
+	private String carteiraDeTrabalho;
+	
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dataDeAdmissao;
+	
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Pessoa pessoa;
+
+	public String getCarteiraDeTrabalho() {
+		return carteiraDeTrabalho;
+	}
+
+	public void setCarteiraDeTrabalho(String carteiraDeTrabalho) {
+		this.carteiraDeTrabalho = carteiraDeTrabalho;
+	}
+
+	public Date getDataDeAdmissao() {
+		return dataDeAdmissao;
+	}
+
+	public void setDataDeAdmissao(Date dataDeAdmissao) {
+		this.dataDeAdmissao = dataDeAdmissao;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+}
